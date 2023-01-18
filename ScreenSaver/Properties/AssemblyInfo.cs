658bf98ddc58b7e90e1c57ff2ec1dc93bb67ecac
fearlessfrog <fearlessfrog@zoomread.com>
2023-01-18 14:28:36 -0800

    Installer and filename

--- ScreenSaver/Properties/AssemblyInfo.cs
+++ ScreenSaver/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScreenSaver")]
+[assembly: AssemblyTitle("MudspikeScreenSaver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScreenSaver")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyProduct("MudspikeScreenSaver")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
